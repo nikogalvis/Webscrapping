c61c4a058256f549af4b5229317f52e002708e79
Nicolas Galvis <nigalviso@unal.edu.co>
2025-07-22 20:12:07 -0500

    Delete Webscrapping directory

--- Webscrapping/__init__.py
+++
@@ -1,2 +0,0 @@
-from .website.document.BaseDoc import Documents
-from .website.document.document_types.DocumentExcel import DocumentExcel
--- Webscrapping/logic/document_logic/document_text_logic.py
+++
@@ -1,9 +0,0 @@
-class Documents:
-  def __init__(self, name: "str", type: "str"):
-    self.type = type
-
-  def create_document(self):
-    return
-  
-  def add_information(self, new_information: "str"):
-    return
